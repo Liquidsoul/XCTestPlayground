f22620c4533631fd05479e25bdc4950ffdfb4cfe
ChrisJeong <crowjdh@gmail.com>
2017-11-04 21:56:05 +0700

    Convert project to support swift 4

--- XCTestPlaygroundExample.playground/Contents.swift
+++ XCTestPlaygroundExample.playground/Contents.swift
@@ -2,10 +2,11 @@
 //:
 //: Note that you **need** to build the framework first using a 64-bit target (iPhone 5s and above) before you can use it here.
 
+import Foundation
 import XCTestPlayground
 
 class TestCase: XCTestCase {
-     func testAssertions() {
+     @objc func testAssertions() {
          XCTAssertEqual(1, 2)
          XCTAssertEqual([1, 2], [2, 3])
          XCTAssertGreaterThanOrEqual(1, 2)
